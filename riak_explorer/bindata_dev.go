3fba3caf29360c0076c6fdb57b92ba7bf3318cac
drewkerrigan <dkerrigan@basho.com>
2015-07-17 14:22:27 -0400

    adding multiple symlinks for dev data

--- riak_explorer/bindata_dev.go
+++ riak_explorer/bindata_dev.go
@@ -1,4 +1,4 @@
 // +build !rel
 
-//go:generate go-bindata -ignore=Makefile -o bindata_generated.go -pkg=riak_explorer -prefix=data/ -debug data/
+//go:generate go-bindata -ignore=Makefile -o bindata_generated.go -pkg=riak_explorer -prefix=data/riak_explorer -debug data/
 package riak_explorer
--- scheduler/bindata_dev.go
+++ scheduler/bindata_dev.go
@@ -1,5 +1,5 @@
 // +build !rel
 
-//go:generate go-bindata -ignore=Makefile -o bindata_generated.go -pkg=scheduler -prefix=data/ -debug data/
+//go:generate go-bindata -ignore=Makefile -o bindata_generated.go -pkg=scheduler -prefix=data/scheduler -debug data/
 
 package scheduler
