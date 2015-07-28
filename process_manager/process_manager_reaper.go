a81892f7b186012301b063c35dc8b8158c58966e
Sargun Dhillon <sargun@sargun.me>
2015-07-27 17:54:20 -0700

    Minor gofmt fixups on process_manager

--- process_manager/process_manager_reaper.go
+++ process_manager/process_manager_reaper.go
@@ -1,10 +1,10 @@
 package process_manager
 
 import (
-	"syscall"
-	"os/signal"
 	log "github.com/Sirupsen/logrus"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func init() {
@@ -13,24 +13,23 @@ func init() {
 	go processManagerLoop()
 }
 
-
 var subscribeNotifications chan subscriptionRequest
 var unsubscribeNotifications chan unsubscriptionRequest
 
-
 type subscriptionRequest struct {
-	pid 	int
+	pid       int
 	replyChan chan pidChangeNotification
 }
 
 type unsubscriptionRequest struct {
-	pid 	int
+	pid int
 }
 type pidChangeNotification struct {
-	pid 	int
+	pid     int
 	wstatus syscall.WaitStatus
 	rusage  syscall.Rusage
 }
+
 func subscribe(pid int) chan pidChangeNotification {
 	// reply must be non-blocking
 	replyChan := make(chan pidChangeNotification, 1)
@@ -56,43 +55,49 @@ func processManagerLoop() {
 
 	for {
 		select {
-		case subscribe := <- subscribeNotifications: {
-			_, assigned := subscriptions[subscribe.pid]
-			if !assigned {
-				subscriptions[subscribe.pid] = subscribe.replyChan
-			} else {
-				panic("Duplicate subscription for a PID")
+		case subscribe := <-subscribeNotifications:
+			{
+				_, assigned := subscriptions[subscribe.pid]
+				if !assigned {
+					subscriptions[subscribe.pid] = subscribe.replyChan
+				} else {
+					panic("Duplicate subscription for a PID")
+				}
 			}
-		}
-		case unsubscribe := <- unsubscribeNotifications: {
-			delete(subscriptions, unsubscribe.pid)
-		}
-		case <- sigchlds: {
-			var wstatus syscall.WaitStatus
-			var rusage syscall.Rusage
-			waitPid, err := syscall.Wait4(-1, &wstatus, 0, &rusage)
-			if err == nil {
-				subscription, assigned := subscriptions[waitPid]
-				if assigned {
-					notification := pidChangeNotification{
-						pid: waitPid,
-						wstatus: wstatus,
-						rusage: rusage,
-					}
-					select {
-					case subscription <- notification: {}
-					default: {
-						log.Info("PID Change subscription channel full, deleting entry")
-						delete(subscriptions, waitPid)
-					}
+		case unsubscribe := <-unsubscribeNotifications:
+			{
+				delete(subscriptions, unsubscribe.pid)
+			}
+		case <-sigchlds:
+			{
+				var wstatus syscall.WaitStatus
+				var rusage syscall.Rusage
+				waitPid, err := syscall.Wait4(-1, &wstatus, 0, &rusage)
+				if err == nil {
+					subscription, assigned := subscriptions[waitPid]
+					if assigned {
+						notification := pidChangeNotification{
+							pid:     waitPid,
+							wstatus: wstatus,
+							rusage:  rusage,
+						}
+						select {
+						case subscription <- notification:
+							{
+							}
+						default:
+							{
+								log.Info("PID Change subscription channel full, deleting entry")
+								delete(subscriptions, waitPid)
+							}
+						}
+					} else {
+						log.Info("PID notification received without actor to send it to: ", waitPid)
 					}
 				} else {
-					log.Info("PID notification received without actor to send it to: ", waitPid)
+					log.Error("Error waiting for PID: ", err)
 				}
-			} else {
-				log.Error("Error waiting for PID: ", err)
 			}
-		}
 		}
 	}
 }
