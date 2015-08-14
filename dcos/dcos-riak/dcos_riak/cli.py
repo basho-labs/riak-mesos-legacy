3e615ed03f59eb99fe285485475ac24ae6aaae37
drewkerrigan <dkerrigan@basho.com>
2015-08-14 11:32:08 -0400

    dcos cli improvements

--- dcos/dcos-riak/dcos_riak/cli.py
+++ dcos/dcos-riak/dcos_riak/cli.py
@@ -25,30 +25,13 @@ import pkg_resources
 from dcos import marathon, util
 from dcos_riak import constants
 
-
-def api_url(name):
-    if name == "":
-        name="riak"
-
-    client = marathon.create_client()
-    tasks = client.get_tasks(name)
-
-    if len(tasks) == 0:
-        usage()
-        print("\nTry running the following to verify that "+ name + " is the name \nof your service instance:\n")
-        print("    dcos service\n")
-        raise CliError("Riak is not running.")
-
-    base_url = util.get_config().get('core.dcos_url').rstrip("/")
-    return base_url + "/service/" + name + "/"
-
 def usage():
     print("dcos riak <subcommand> [<options>]")
     print("Subcommands: ")
     print("    --get-clusters")
-    print("    --get-nodes <cluster-name>")
-    print("    --get-nodes <cluster-name> <node>")
+    print("    --get-cluster <cluster-name>")
     print("    --create-cluster <cluster-name>")
+    print("    --get-nodes <cluster-name>")
     print("    --add-node <cluster-name>")
     print("    --info")
     print("    --version")
@@ -63,94 +46,148 @@ def maybe_debug(flag, r):
         print("Text: "+ r.text)
         print("[/DEBUG]")
 
-def run(args):
-    help_arg = len(args) > 0 and args[0] == "help"
-    if help_arg:
-        usage()
-        return 0
+def api_url(name):
+    client = marathon.create_client()
+    tasks = client.get_tasks(name)
 
-    service_url = ""
+    if len(tasks) == 0:
+        usage()
+        print("\nTry running the following to verify that "+ name + " is the name \nof your service instance:\n")
+        print("    dcos service\n")
+        raise CliError("Riak is not running, try with --framework-name <framework-name>.")
+
+    base_url = util.get_config().get('core.dcos_url').rstrip("/")
+    return base_url + "/service/" + name + "/"
+
+def validate_arg(opt, arg):
+    if arg.startswith("-"):
+        raise CliError("Invalid argument for opt: " + opt + " [" + arg + "].")
+
+def format_json_array_keys(description, json_str, failure):
+    try:
+        obj_arr = json.loads(json_str)
+        print(description + "[" + ', '.join(obj_arr.keys()) + "]", end="")
+    except:
+        print(description + failure)
+
+def format_json_value(description, json_str, key, failure):
+    try:
+        obj = json.loads(json_str)
+        print(description + obj[key], end="")
+    except:
+        print(description + failure)
+
+def get_clusters(service_url, flag):
+    r = requests.get(service_url + "clusters")
+    maybe_debug(flag, r)
+    if r.status_code == 200:
+        format_json_array_keys("Clusters: ", r.text, "[]")
+    else:
+        print("No clusters created")
+
+def get_cluster(service_url, name, flag):
+    r = requests.get(service_url + "clusters/" + name)
+    maybe_debug(flag, r)
+    if r.status_code == 200:
+        format_json_value("Cluster: ", r.text, "Name", "Error getting cluster.")
+    else:
+        print("Cluster not created.")
+
+def create_cluster(service_url, name, flag):
+    r = requests.post(service_url + "clusters/" + name, data="")
+    maybe_debug(flag, r)
+    if r.text == "" or r.status_code != 200:
+        print("Cluster already exists")
+    else:
+        format_json_value("Added cluster: ", r.text, "Name", "Error creating cluster.")
+
+def get_nodes(service_url, name, flag):
+    r = requests.get(service_url + "clusters/" + name + "/nodes")
+    maybe_debug(flag, r)
+    format_json_array_keys("Nodes: ", r.text, "[]")
+
+def add_node(service_url, name, flag):
+    r = requests.post(service_url + "clusters/" + name + "/nodes", data="")
+    maybe_debug(flag, r)
+    format_json_value("New node: ", r.text, "UUID", "Error adding node.")
+
+def run(args):
+    help_arg = 0
     flag = 0
+    service_url = ""
+
+    if "--help" in args or "-h" in args:
+        help_arg = 1
 
     if "--debug" in args:
         flag = 1
 
+    if help_arg:
+        usage()
+        return 0
+
+    if "--info" in args and "--framework-name" not in args:
+        print("Start and manage Riak nodes")
+        return 0
+
+    if "--version" in args:
+        print(constants.version)
+        return 0
+
+    if "--config-schema" in args:
+        print("{}")
+        return 0
+
     if "--framework-name" in args and args.index('--framework-name')+1 < len(args):
-        service_url = api_url(args[args.index('--framework-name')+1])
-        if args.index('--framework-name')+2 == len(args):
-            print("Service URL: " + service_url)
+        name = args[args.index('--framework-name')+1]
+        validate_arg("--framework-name", name)
+        service_url = api_url(name)
     else:
-        service_url = api_url("")
+        service_url = api_url("riak")
+
+    if "--info" in args:
+        print("Start and manage Riak nodes")
+        print("Service URL: " + service_url)
+        return 0
 
     for i, opt in enumerate(args):
-        if opt == "--get-cluster":
-            if i+1 < len(args):
-                r = requests.get(service_url + "clusters/" + args[i+1])
-                maybe_debug(flag, r)
-                if r.status_code == 200:
-                    print("Cluster: "+ r.text, end="\n")
-                    r = requests.get(service_url + "clusters/" + args[i+1] + "/nodes")
-                    maybe_debug(flag, r)
-                    try:
-                        nodes = json.loads(r.text)
-                        print("Nodes: [" + ', '.join(nodes.keys()) + "]", end="")
-                        break
-                    except:
-                        print("Nodes: []")
-                else:
-                    print("Cluster not created")
-            else:
-                usage()
-
         if opt == "--get-clusters":
-            r = requests.get(service_url + "clusters")
-            maybe_debug(flag, r)
-            if r.status_code == 200:
-                try:
-                    clusters = json.loads(r.text)
-                    print("Clusters: [" + ', '.join(clusters.keys()) + "]", end="")
-                    break
-                except:
-                    print("[]")
-            else:
-                print("No clusters created")
-
-        if opt == "--get-nodes":
+            get_clusters(service_url, flag)
+            break
+        elif opt == "--get-cluster":
             if i+1 < len(args):
-                r = requests.get(service_url + "clusters/" + args[i+1] + "/nodes")
-                maybe_debug(flag, r)
-                try:
-                    nodes = json.loads(r.text)
-                    print("Nodes: [" + ', '.join(nodes.keys()) + "]", end="")
-                    break
-                except:
-                    print("Nodes: []")
+                validate_arg(opt, args[i+1])
+                get_cluster(service_url, args[i+1], flag)
+                print("")
+                get_nodes(service_url, args[i+1], flag)
             else:
                 usage()
-
-        if opt == "--create-cluster":
+            break
+        elif opt == "--get-nodes":
             if i+1 < len(args):
-                r = requests.post(service_url + "clusters/" + args[i+1], data="")
-                maybe_debug(flag, r)
-                if r.text == "" or r.status_code != 200:
-                    print("Cluster already exists")
-                else:
-                    print("Created cluster: "+ r.text, end="\n")
+                validate_arg(opt, args[i+1])
+                get_nodes(service_url, args[i+1], flag)
             else:
                 usage()
-
-        if opt == "--add-node":
+            break
+        elif opt == "--create-cluster":
             if i+1 < len(args):
-                r = requests.post(service_url + "clusters/" + args[i+1] + "/nodes", data="")
-                maybe_debug(flag, r)
-                try:
-                    node = json.loads(r.text)
-                    print("Added node: " + node["UUID"], end="")
-                    break
-                except:
-                    print("Error adding node.")
+                validate_arg(opt, args[i+1])
+                create_cluster(service_url, args[i+1], flag)
             else:
                 usage()
+            break
+        elif opt == "--add-node":
+            if i+1 < len(args):
+                validate_arg(opt, args[i+1])
+                add_node(service_url, args[i+1], flag)
+            else:
+                usage()
+            break
+        else:
+            usage()
+            print("")
+            raise CliError("Unrecognized option: " + opt)
 
     print("")
 
@@ -166,27 +203,6 @@ def main():
     if len(args) == 0:
         usage()
         return 0
-
-    if len(args) == 1 and args[0] == "--info":
-        print("Start and manage Riak nodes")
-        return 0
-
-    if len(args) == 1 and args[0] == "--version":
-        print(constants.version)
-        return 0
-
-    if len(args) == 1 and args[0] == "--config-schema":
-        print("{}")
-        return 0
-
-    if "--help" in args or "-h" in args:
-        if "--help" in args:
-            args.remove("--help")
-
-        if "-h" in args:
-            args.remove("-h")
-
-        args.insert(0, "help")
 
     try:
         return run(args)
