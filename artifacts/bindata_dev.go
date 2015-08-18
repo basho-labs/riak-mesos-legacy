03333802c6563cb9d4742cbc4bd0630d8bcfc632
Sargun Dhillon <sargun@sargun.me>
2015-08-18 03:56:15 -0700

    Small fix to not include source in bindata

--- artifacts/bindata_dev.go
+++ artifacts/bindata_dev.go
@@ -1,5 +1,5 @@
 // +build !rel
 
-//go:generate go-bindata -ignore=Makefile|deploy_key|deploy_keypub -o bindata_generated.go -pkg=artifacts -prefix=data/ -debug data/
+//go:generate go-bindata -ignore=Makefile|deploy_key|deploy_keypub -o bindata_generated.go -pkg=artifacts -prefix=data/ -debug data/riak_explorer-bin.tar.gz data/riak-2.1.1-bin.tar.gz data/trusty.tar.gz
 
 package artifacts
--- artifacts/bindata_rel.go
+++ artifacts/bindata_rel.go
@@ -1,5 +1,5 @@
 // +build rel
 
-//go:generate go-bindata -ignore=Makefile|deploy_key|deploy_keypub  -o bindata_generated.go -pkg=artifacts -prefix=data/ data/
+//go:generate go-bindata  -o bindata_generated.go -pkg=artifacts -prefix=data/ data/riak_explorer-bin.tar.gz data/riak-2.1.1-bin.tar.gz data/trusty.tar.gz
 
 package artifacts
