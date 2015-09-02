c7df3e393c4da0368bf8c2c70ee0c514ad0d75b0
drewkerrigan <dkerrigan@basho.com>
2015-09-02 09:54:25 -0400

    fixing ordering stuff for travis build

--- cepmd/cepm/bindata_rel.go
+++ cepmd/cepm/bindata_rel.go
@@ -2,4 +2,5 @@
 
 package cepm
 
-//go:generate go-bindata -ignore=Makefile -o bindata_generated.go -pkg=cepm -prefix=data -tags=rel data
+//go:generate go-bindata -ignore=Makefile -o bindata_generated.go -pkg=cepm -prefix=data data
+import _ "github.com/jteeuwen/go-bindata"
