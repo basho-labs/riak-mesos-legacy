314ea1d0834d87d1f3fe538ec14743b9a0640b0b
Sargun Dhillon <sargun@sargun.me>
2015-09-18 18:42:46 -0700

    Remove Zookeeper locking logic

--- metadata_manager/metadata_manager_framework.go
+++ metadata_manager/metadata_manager_framework.go
@@ -6,12 +6,9 @@ import (
 )
 
 func (mgr *MetadataManager) SetupFramework(URI string) {
-	err := mgr.zkLock.Lock()
-	if err != nil {
-		log.Panic("Unable to get framework lock: ", err)
-	}
+
 	URIPath := makeSubSpace(mgr.namespace, "uri")
-	_, err = mgr.zkConn.Create(URIPath.GetZKPath(), []byte(URI), zk.FlagEphemeral, zk.WorldACL(zk.PermAll))
+	_, err := mgr.zkConn.Create(URIPath.GetZKPath(), []byte(URI), zk.FlagEphemeral, zk.WorldACL(zk.PermAll))
 	if err != nil {
 		log.Panic(err)
 	}
