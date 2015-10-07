3ba28371c2917a26873e28809f9a4f72f7e79658
drewkerrigan <dkerrigan@basho.com>
2015-10-07 12:36:31 -0400

    changes to provision scripts

--- provision.sh
+++ provision.sh
@@ -10,12 +10,21 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E9
 echo "deb https://get.docker.io/ubuntu docker main" | sudo tee /etc/apt/sources.list.d/docker.list
 apt-get -y update > /dev/null
 apt-get -y upgrade > /dev/null
+apt-get -y install software-properties-common
+add-apt-repository -y ppa:openjdk-r/ppa
+apt-get -y update > /dev/null
 apt-get -y install make git gcc g++ curl
 apt-get -y install python-dev libcppunit-dev libunwind8-dev autoconf autotools-dev libltdl-dev libtool autopoint libcurl4-openssl-dev libsasl2-dev
-apt-get -y install openjdk-7-jdk zookeeperd default-jre python-setuptools python-protobuf
+apt-get -y install openjdk-8-jdk default-jre python-setuptools python-protobuf
+update-java-alternatives -s /usr/lib/jvm/java-1.8.0-openjdk-amd64
 apt-get -y install libprotobuf-dev protobuf-compiler
 apt-get -y install mesos marathon
 apt-get -y install lxc-docker
+
+service zookeeper restart
+service mesos-slave restart
+service mesos-master restart
+service marathon restart
 
 # Install Golang
 apt-get -y update
