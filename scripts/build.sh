949ec84b40e9fbdab0295e0572ec6746796a6086
Drew Kerrigan <dr.kerrigan@gmail.com>
2016-01-17 17:07:03 -0500

    updating build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-export REAL_GOPATH=$GOPATH
-
+# Erlang init
 . $HOME/erlang/R16B02-basho8/activate
+
+# Golang init
+export REAL_GOPATH=$GOPATH
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
 gvm use go1.5
 export GOPATH=$REAL_GOPATH
 export PATH=$PATH:$GOPATH/bin:$HOME/.gvm/gos/go1.4/bin
 
+# Get switch to branch
 cd $GOPATH/src/github.com/basho-labs/riak-mesos
+git checkout $BRANCH && git pull
+
+# Cleanup, compile, package
+make clean
 make
+make package
