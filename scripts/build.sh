a5351e2d2fc7fff78abb274d44f8a7c3ab7fa3bb
Drew Kerrigan <dr.kerrigan@gmail.com>
2016-01-17 16:51:24 -0500

    fixing gopath

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+export REAL_GOPATH=$GOPATH
+
 . $HOME/erlang/R16B02-basho8/activate
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
 gvm use go1.5
+export GOPATH=$REAL_GOPATH
 export PATH=$PATH:$GOPATH/bin:$HOME/.gvm/gos/go1.4/bin
 
 cd $GOPATH/src/github.com/basho-labs/riak-mesos
