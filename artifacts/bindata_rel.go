d88cd36551091e9fc37eb4ce3694b98069c95111
Drew Kerrigan <dr.kerrigan@gmail.com>
2016-01-18 00:21:33 -0500

    adding cepmd to bindata

--- artifacts/bindata_rel.go
+++ artifacts/bindata_rel.go
@@ -1,5 +1,5 @@
 // +build rel
 
-//go:generate go-bindata -o bindata_generated.go -pkg=artifacts -prefix=data/ data/riak_mesos_executor.tar.gz data/riak.conf data/advanced.config data/riak-bin.tar.gz
+//go:generate go-bindata -o bindata_generated.go -pkg=artifacts -prefix=data/ data/riak_mesos_executor.tar.gz data/riak.conf data/advanced.config data/riak-bin.tar.gz data/cepmd_linux_amd64
 
 package artifacts
